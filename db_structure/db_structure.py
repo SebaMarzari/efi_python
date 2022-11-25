48a8480bfc968d904de240f97d3a76a908e0f34d
SebaMarzari <sebastian.marzari@emprinet.com>
2022-11-24 21:43:06 -0300

    Delete d_structure

--- db_structure/db_structure.py
+++
@@ -1,9 +0,0 @@
-# class User(db.Model):
-#     __tablename__ = 'user'
-
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(100), nullable=True, unique=True)
-#     password = db.Column(db.String(100), nullable=True)
-#     idUserType = db.Column(db.Integer, ForeignKey('userType.id'))
-#     fCarga = db.Column(db.TIMESTAMP, nullable=True)
-#     idPerson = db.Column(db.Integer, ForeignKey('person.id'))
